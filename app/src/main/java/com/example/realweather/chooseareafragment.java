405d9da7974bc43ede14a39c90a2abaa9f75e254
GabageCollection <842545300@qq.com>
2020-10-08 21:52:29 +0800

    提交

--- app/src/main/java/com/example/realweather/chooseareafragment.java
+++ app/src/main/java/com/example/realweather/chooseareafragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -12,14 +13,18 @@ import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import org.litepal.LitePal;
+import org.litepal.crud.LitePalSupport;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+
 
 import db.city;
 import db.county;
 import db.province;
-import kotlin.internal.UProgressionUtilKt;
+
+
 
 public class chooseareafragment extends Fragment {
     public static final int LEVEL_PROVINCE =0;
@@ -34,11 +39,12 @@ public class chooseareafragment extends Fragment {
     private List<province>provinceList;
     private List<city>cityList;
     private List<county>countyList;
-    private province selectprovince;
-    private city seletcity;
-    private county selectcounty;
+    private province selectedprovince;
+    private city selectedcity;
+    private county selectedcounty;
     private int currentLevel;
 
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.chooose_area,container,false);
         TextView titleText = (TextView) view.findViewById(R.id.title_text);
@@ -48,4 +54,54 @@ public class chooseareafragment extends Fragment {
         listView.setAdapter(adapter);
         return view;
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (currentLevel == LEVEL_PROVINCE) {
+                    selectedprovince = provinceList.get(position);
+                    querycities();
+                } else if (currentLevel == LEVEL_CITY) {
+                    selectedcity = cityList.get(position);
+                    querycounties();
+                }
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (currentLevel == LEVEL_COUNTY){
+                    querycities();
+                }else if(currentLevel==LEVEL_CITY){
+                    queryprovinces();
+                }
+            }
+        };
+        queryprovinces();
+    }
+
+    private void queryprovinces(){
+        titleText.setText("中国");
+        backButton.setVisibility(View.GONE);
+        provinceList = LitePal.findAll(province.class);
+        if(provinceList.size()>0){
+            dataList.clear();
+            for(province province:provinceList){
+                dataList.add(province.getProvinceName());
+            }
+            adapter.notifyDataSetChanged();
+            listview.setSelection(0);
+            currentLevel=LEVEL_PROVINCE;
+        }else{
+            String address = "http://guolin.tech/api/china";
+            queryFormServer(addresss,"province");
+        }
+    }
 }
+
+
+
+
